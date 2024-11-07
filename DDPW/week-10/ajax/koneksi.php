830b4fa12c4e7ce9a2ed91f56bad43dcd36ba0de
KhenCahyo13 <khencahyo02@gmail.com>
2024-11-07 17:07:42 +0700

    DDPW Jobsheet 10

---
+++ DDPW/week-10/ajax/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', 'root');
+    define('DB', 'week10-ddpw');
+
+    $mydb = new mysqli(HOST, USER, PASS, DB);
+?>

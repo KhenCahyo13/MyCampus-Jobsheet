155a98674bddf9f1252daaf44b7ddf2c55d26ba6
KhenCahyo13 <khencahyo02@gmail.com>
2025-02-16 08:05:45 +0700

    PWL - Web Framework (Week-1)

---
+++ web-lanjut/week-1/PWL-2025/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel PWL 2025</title>
+</head>
+<body>
+    <h1>Advanced Web Programming 2025</h1>
+    <p>Passion for learning Laravel 11</p>
+</body>
+</html>
